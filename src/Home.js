734b38bd1cfb373ac739205501c787b893c925fc
Joseff361 <Joseff361@outlook.com>
2022-06-07 11:01:34 -0500

    Outputting Lists

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,26 @@
 import { useState } from 'react';
 
 const Home = () => {
-  //let name = 'Mario';
-
-  const [name, setName] = useState('Mario');
-  const [age, setAge] = useState(25);
-
-  const handleClick = e => {
-    setName('Luigi');
-    setAge(30);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    {
+      title: 'Web dev top tips',
+      body: 'lorem ipsum...',
+      author: 'mario',
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Home Page</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map(blog => (
+        // The key attribute is used by React to track each element inside an array and distinguish between them
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/dump/Hook.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const Home = () => {
+  //let name = 'Mario';
+
+  const [name, setName] = useState('Mario');
+  const [age, setAge] = useState(25);
+
+  const handleClick = e => {
+    setName('Luigi');
+    setAge(30);
+  };
+
+  return (
+    <div className="home">
+      <h2>Home Page</h2>
+      <p>
+        {name} is {age} years old
+      </p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+
+export default Home;
