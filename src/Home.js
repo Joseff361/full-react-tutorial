a340b40febe01d89b61a497985a60ce4d19b9561
Joseff361 <Joseff361@outlook.com>
2022-06-07 09:26:40 -0500

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+  const handleClick = e => {
+    console.log('Hello ninjas', e);
+  };
+
+  const handleClickMeAgain = (name, e) => {
+    console.log('Hello, ' + name, e);
+  };
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      {/* Just a reference of the function. React will execute the function */}
+      <button onClick={handleClick}>Click me</button>
+      {/* Wrapping in anonymous function to pass paramenters */}
+      <button onClick={e => handleClickMeAgain('Mario', e)}>
+        Click me again
+      </button>
     </div>
   );
 };
