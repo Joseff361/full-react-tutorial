f2cd813cf091232030b0fde2aecdab418752104f
Joseff361 <Joseff361@outlook.com>
2022-06-09 23:37:47 -0500

    Fetching Data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,5 @@
 // Stateless Functional Component
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -8,7 +8,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -2,36 +2,25 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
-  const [name, setName] = useState('mario');
-
-  const handleDelete = id => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  // This hook will run every single time when the components re-render
   useEffect(() => {
-    console.log('useEffect');
-  }, [name]); // If you add a empty array, this fun only runs once
+    const fetchBlogs = async () => {
+      const res = await fetch(' http://localhost:8000/blogs');
+      const data = await res.json();
+      setBlogs(data);
+    };
+
+    try {
+      fetchBlogs();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <button onClick={() => setName(name === 'mario' ? 'luigi' : 'mario')}>
-        Change name
-      </button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
