96b409682c65810a8da1fd73d6745f3903ea8ad7
Joseff361 <Joseff361@outlook.com>
2022-06-09 22:44:25 -0500

    useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -13,14 +13,25 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = id => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   };
 
+  // This hook will run every single time when the components re-render
+  useEffect(() => {
+    console.log('useEffect');
+  }, [name]); // If you add a empty array, this fun only runs once
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName(name === 'mario' ? 'luigi' : 'mario')}>
+        Change name
+      </button>
+      <p>{name}</p>
     </div>
   );
 };
